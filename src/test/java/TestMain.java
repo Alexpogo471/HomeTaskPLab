810d43aa118db336e219495f8e19986cb40705e3
Alexey Pogorelov <aieks.p@rambler.ru>
2019-12-19 20:58:10 +0300

    add .gitIgnore

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -3,7 +3,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.internal.reflect.MethodMatcherException;
 
 
 public class TestMain {
@@ -23,7 +22,7 @@ public class TestMain {
 
     //--------------- Successful Tests -------------------
 
-    @DataProvider(name = "DecimalBinaryProvider", parallel = true)
+    @DataProvider(name = "DecimalBinaryProvider")
     public Object[][] DecimalBinaryProvider() {
         return new Object[][]{
                 {145, "10010001"}, {67, "1000011"}, {33, "100001"}, {112, "1110000"}
@@ -35,7 +34,7 @@ public class TestMain {
         Assert.assertEquals(main.conversionDecimalToBinary(number), result, "wrong conversion");
     }
 
-    @DataProvider(name = "DecimalToHexProvider", parallel = true)
+    @DataProvider(name = "DecimalToHexProvider")
     public Object[][] DecimalToHexProvider() {
         return new Object[][]{
                 {58, "3a"}, {144, "90"}, {298, "12a"}, {12, "c"}
@@ -47,7 +46,7 @@ public class TestMain {
         Assert.assertEquals(main.conversionDecimalToHexadecimal(number), result, "wrong conversion");
     }
 
-    @DataProvider(name = "BinaryToOctalProvider", parallel = true)
+    @DataProvider(name = "BinaryToOctalProvider")
     public Object[][] BinaryToOctalProvider() {
         return new Object[][]{
                 {101010, "52"}, {101, "5"}, {11010, "32"}, {111101001, "751"}
@@ -59,7 +58,7 @@ public class TestMain {
         Assert.assertEquals(main.conversionBinaryToOctal(number), result, "wrong conversion");
     }
 
-    @DataProvider(name = "validateNumberProvider", parallel = true)
+    @DataProvider(name = "validateNumberProvider")
     public Object[][] validateNumberProvider() {
         return new Object[][]{
                 {"543", true}, {"34", true}, {"1010101", true}, {"12a", true}
@@ -73,7 +72,7 @@ public class TestMain {
 
     //--------------- Failed Tests -------------------
 
-    @DataProvider(name = "DecimalBinaryProviderFailed", parallel = true)
+    @DataProvider(name = "DecimalBinaryProviderFailed")
     public Object[][] DecimalBinaryProviderFailed() {
         return new Object[][]{
                 {0232, "10010001"}
@@ -85,7 +84,7 @@ public class TestMain {
         Assert.assertEquals(main.conversionDecimalToBinary(number), result, "wrong conversion");
     }
 
-    @DataProvider(name = "BinaryToOctalProviderFailed", parallel = true)
+    @DataProvider(name = "BinaryToOctalProviderFailed")
     public Object[][] BinaryToOctalProviderFailed() {
         return new Object[][]{
                 {5344, "5"}
@@ -98,7 +97,7 @@ public class TestMain {
 
     }
 
-    @DataProvider(name = "validateNumberProviderFailed", parallel = true)
+    @DataProvider(name = "validateNumberProviderFailed")
     public Object[][] validateNumberProviderFailed() {
         return new Object[][]{
                 {"56hgmn", true}
